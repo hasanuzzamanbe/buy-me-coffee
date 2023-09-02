2b294d18fc7384b267d615bd02ad9cebe6999fd4
Hasanuzzaman <hasanuzzamanbe@gmail.com>
2023-09-02 21:51:14 +0600

    frontend style update

--- includes/views/user/payment.php
+++ includes/views/user/payment.php
@@ -7,21 +7,52 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php esc_html_e('Buy Me Coffee', 'buymecoffee') ?></title>
     <?php
-    wp_head();
+        wp_head();
     ?>
-    <style type="text/css" lang="scss">
+    <style type="text/css">
         .buymecoffee_preview_body {
-            width: 100%;
+          width: 90%;
+          display: flex;
+          flex-direction: row-reverse;
+          justify-content: space-between;
+          max-width: 900px;
+          margin: 0 auto;
         }
+        #bmc_modal_wrapper::-webkit-scrollbar {
+          display: none;
+        }
+          @media (max-width: 960px) {
+                .buymecoffee_preview_body {
+                      flex-direction: column;
+                }
+          }
 
         .bmc_appreciation_title {
             font-weight: 100;
             margin: 0;
         }
+        h3.buymecoffee_form_to {
+          margin: 10px 0px;
+          font-size: 30px;
+          color: #6d6d6d;
+        }
+
+        span.buymecoffee_form_to {
+          color: #c0c0c0;
+        }
+
+        .buymecoffee_your_content_wrapper {
+          display: flex;
+          align-content: stretch;
+          align-items: center;
+        }
+
         .buymecoffee_form_preview_wrapper {
-            max-width: 555px;
+            max-width: 400px;
             background: white;
-            margin: auto;
+            width: 70%;
+            border-left: 1px solid #f4f4f4;
+            padding: 0px 36px;
         }
 
         .buymecoffee_preview_header {
@@ -47,14 +78,16 @@
         }
         .wpm_bmc_input_content input,
         .wpm_bmc_input_content textarea {
-            width: 100% !important;
+            width: 98% !important;
             padding: 4px;
-            border: 1px solid #d1d3da;
+            border: 1px solid #e9e9e9;
             border-radius: 4px;
             resize: vertical;
             box-shadow: none;
             background: white !important;
-            height: 33px;
+        }
+        .wpm_bmc_input_content input{
+            height: 30px;
         }
         .wpm_bmc_input_content input:focus,
         .wpm_bmc_input_content textarea:focus {
@@ -68,7 +101,7 @@
 
         .wpm_bmc_currency_prefix {
             font-size: 33px;
-            background: #000000;
+            background: #ffc568;
             border-top-left-radius: 6px;
             border-bottom-left-radius: 6px;
             color: white;
@@ -88,32 +121,72 @@
             min-height: 38px;
             cursor: pointer;
         }
+        .buymecoffee_profile_section_image {
+          display: flex;
+        }
+        .buymecoffee_profile_section_image img {
+            border-radius: 50%;
+            width: 130px;
+            height: 130px;
+            border: 4px solid #dadada;
+            margin: 0 auto;
+            z-index: 999;
+            background: #fff;
+            margin-top: -65px;
+        }
+        .buymecoffee_profile_hr {
+            height: 80px;
+            width: 100%;
+            background: #fff7eb;
+        }
+        .wpm_buymecoffee_no_signup{
+            color: #b8b8b8;
+            font-size: 12px;
+            font-family: sans-serif;
+            text-align: center;
+        }
     </style>
-</head>
-<body>
 <div id="buymecoffee_preview_top">
+    <div class="buymecoffee_profile_section">
+        <div class="buymecoffee_profile_hr">
+        </div>
+        <div class="buymecoffee_profile_section_image">
+            <img src="<?php echo BUYMECOFFEE_URL . 'assets/images/coffee.png';?>"
+                 alt="Profile Image">
+        </div>
+
+    </div>
     <div class="buymecoffee_preview_header">
         <h3><?php esc_html_e('', 'buymecoffee') ?></h3>
     </div>
     <div class="buymecoffee_preview_body">
         <div class="buymecoffee_form_preview_wrapper">
-            <div style="margin-bottom:23px;">
-                <h4 class="bmc_appreciation_title">Thanks for your appreciation.</h4>
-            </div>
+            <h3 class="buymecoffee_form_to">
+                Buy
+                <span class="buymecoffee_form_to"><?php esc_html_e($template['yourName'], 'buymecoffee') ?></span> <br/>
+                a coffee
+            </h3>
             <?php
                 $form =  \buyMeCoffee\Builder\Render::renderInputElements();
                 echo $form;
             ?>
+        </div>
+        <div class="buymecoffee_your_content_wrapper">
+            <div class="buymecoffee_your_content">
+                <div class="buymecoffee_your_content_title">
+                    <div style="margin-bottom:23px;">
+                        <h4 class="bmc_appreciation_title">Thanks for your appreciation.</h4>
+                    </div>
+                </div>
+                <div class="buymecoffee_your_content_body">
+                    <p></p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
 <?php
 wp_footer();
 ?>
-
-<script type="text/javascript">
-
-</script>
-
 </body>
 </html>
