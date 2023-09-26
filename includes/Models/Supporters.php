82387e2988bb1d74c76864adcfcf24a3f27e0210
Hasanuzzaman <hasanuzzamanbe@gmail.com>
2023-09-26 17:46:19 +0600

    update

--- includes/Models/Supporters.php
+++ includes/Models/Supporters.php
@@ -12,8 +12,6 @@ class Supporters
 
         $query = wpmBmcDB()->table('wpm_bmc_supporters')
             ->orderBy('id', 'DESC')
-            ->select('*')
-            ->select(wpmBmcDB()->raw('DATE_FORMAT(created_at, "%e/%m/%y at %H:%i")  as created_at'))
             ->offset($offset)
             ->limit($args['posts_per_page']);
 
