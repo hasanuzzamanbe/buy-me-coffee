2b0668df73fd838ef4e4906fe9d6e9c2213e6421
Hasanuzzaman <hasanuzzamanbe@gmail.com>
2024-03-11 23:41:02 +0600

    Fixes customizer issue, code refactores

--- includes/views/templates/BasicTemplate.php
+++ includes/views/templates/BasicTemplate.php
@@ -15,27 +15,23 @@
     <?php
     if (current_user_can('manage_options')):
         ?>
-        <div class="controller">
+        <div class="buymecoffee_customizer_menu">
             <p>customizer</p>
             <svg fill="#545454" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="18" height="18"><path d="M1,4.75H3.736a3.728,3.728,0,0,0,7.195,0H23a1,1,0,0,0,0-2H10.931a3.728,3.728,0,0,0-7.195,0H1a1,1,0,0,0,0,2ZM7.333,2a1.75,1.75,0,1,1-1.75,1.75A1.752,1.752,0,0,1,7.333,2Z"/><path d="M23,11H20.264a3.727,3.727,0,0,0-7.194,0H1a1,1,0,0,0,0,2H13.07a3.727,3.727,0,0,0,7.194,0H23a1,1,0,0,0,0-2Zm-6.333,2.75A1.75,1.75,0,1,1,18.417,12,1.752,1.752,0,0,1,16.667,13.75Z"/><path d="M23,19.25H10.931a3.728,3.728,0,0,0-7.195,0H1a1,1,0,0,0,0,2H3.736a3.728,3.728,0,0,0,7.195,0H23a1,1,0,0,0,0-2ZM7.333,22a1.75,1.75,0,1,1,1.75-1.75A1.753,1.753,0,0,1,7.333,22Z"/></svg>
         </div>
-        <div class="wrapper" style="display: none;">
-            <ul class="tab__content">
-                <li class="active">
-                    <div class="content__wrapper">
-                        <ul class="colors">
-                            <li data-color="rgb(255, 129, 63)"></li>
-                            <li data-color="rgb(255, 95, 95)"></li>
-                            <li data-color="rgb(95, 127, 255)"></li>
-                            <li data-color="rgb(137, 239, 232)"></li>
-                            <li data-color="rgb(189, 95, 255)"></li>
-                            <li data-color="rgb(244, 113, 255)"></li>
-                            <li data-color="rgb(38, 176, 161)"></li>
-                            <li data-color="rgb(253, 181, 0)"></li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
+        <div class="buymecoffee_customizer_wrapper" style="display: none;">
+            <div class="buymecoffee_color_wrapper">
+                <ul class="buymecoffee_colors">
+                    <li data-color="rgb(255, 129, 63)"></li>
+                    <li data-color="rgb(255, 95, 95)"></li>
+                    <li data-color="rgb(95, 127, 255)"></li>
+                    <li data-color="rgb(137, 239, 232)"></li>
+                    <li data-color="rgb(189, 95, 255)"></li>
+                    <li data-color="rgb(244, 113, 255)"></li>
+                    <li data-color="rgb(38, 176, 161)"></li>
+                    <li data-color="rgb(253, 181, 0)"></li>
+                </ul>
+            </div>
             <div>
                 <button id="buymecoffee_save_changes"> Save Changes</button>
             </div>
--- src/js/customizer.js
+++ src/js/customizer.js
@@ -1,79 +1,102 @@
 jQuery(document).ready(function($) {
-    let unsavedChanges = false;
-    // Variables
-    const colorButton = $(".tab__content .colors li");
-    colorButton.each(function() {
-        const color = $(this).attr('data-color');
-        $(this).css('background-color', color);
-    });
+    const colorButton = $(".buymecoffee_color_wrapper .buymecoffee_colors li");
 
+    let visibleCustomizer = false;
     let buttonColor = null;
     let borderColor = null;
     let bgColor = null;
     let quote = null;
 
-    colorButton.on("click", function () {
-        $(".colors > li").removeClass("active-color");
-        $(this).addClass("active-color");
+    initColors();
+    customizerMenuRegister();
+    quotesEditRegister();
+    saveActionRegister();
+    colorSwitcherRegister();
 
-        buttonColor = $(this).attr("data-color");
-        borderColor = rgbToRgba(buttonColor, '25%');
-        bgColor = rgbToRgba(buttonColor, '5%');
-
-         let groupStyle ={
-            "background-color" : bgColor,
-            'border-color': borderColor
-        };
-
-        $("#buymecoffee_preview_top .buymecoffee_profile_hr").css("background-color", borderColor);
-        $("#buymecoffee_preview_top .buymecoffee_payment_item .buymecoffee_payment_input_content").css(groupStyle);
-        $("#buymecoffee_preview_top button#wpm_submit_button")
-            .attr('style', $(this).attr('style') + 'background-color:' + buttonColor + '!important;');
-    });
-
-
-
-    $('#buymecoffee_save_changes').on("click", function () {
-        $.post(window.buymecoffee_general.ajax_url, {
-            action: 'buymecoffee_admin_ajax',
-            route: 'save_form_design',
-            buymecoffee_nonce: window.buymecoffee_general.buymecoffee_nonce,
-            data: {
-                button_style: buttonColor,
-                bg_style: bgColor,
-                border_style: borderColor,
-                quote: quote
-            }
-        })
-            .then(res => {
-                hideSettings();
-            }).catch (error => {
-            alert(error?.responseJSON?.data?.message);
+    function initColors() {
+        colorButton.each(function() {
+            const color = $(this).attr('data-color');
+            $(this).css('background-color', color);
         });
-    });
+    }
+    function colorSwitcherRegister() {
+        colorButton.on("click", function () {
+            $(".buymecoffee_colors > li").removeClass("active-color");
+            $(this).addClass("active-color");
 
-    $('.controller').on("click", function () {
-        $('.wrapper, .buymecoffee_edit_action_wrapper').toggle();
-    });
+            buttonColor = $(this).attr("data-color");
+            borderColor = rgbToRgba(buttonColor, '25%');
+            bgColor = rgbToRgba(buttonColor, '5%');
 
-    $('.buymecoffee_edit_action, .bmc_appreciation_title').on("click", function () {
-        $('.buymecoffee_edit_action_wrapper blockquote').toggle();
-        $('.buymecoffee_quote_section .buymecoffee_main_quote').toggle();
-    })
+            let groupStyle ={
+                "background-color" : bgColor,
+                'border-color': borderColor
+            };
 
-    $('.buymecoffee_edit_action_wrapper input').on('input', function() {
-        $('.buymecoffee_quote_section .buymecoffee_main_quote p')[0]
-            .innerHTML = quote = $(this).val();
-    });
+            $(".buymecoffee_profile_hr").css("background-color", borderColor);
+            $(".buymecoffee_payment_item .buymecoffee_payment_input_content").css(groupStyle);
+            $("button#wpm_submit_button")
+                .attr('style', $(this).attr('style') + 'background-color:' + buttonColor + '!important;');
+        });
+    }
 
+    function saveActionRegister() {
+        $('#buymecoffee_save_changes').on("click", function () {
+            $.post(window.buymecoffee_general.ajax_url, {
+                action: 'buymecoffee_admin_ajax',
+                route: 'save_form_design',
+                buymecoffee_nonce: window.buymecoffee_general.buymecoffee_nonce,
+                data: {
+                    button_style: buttonColor,
+                    bg_style: bgColor,
+                    border_style: borderColor,
+                    quote: quote
+                }
+            })
+                .then(res => {
+                    hideSettings();
+                }).catch (error => {
+                alert(error?.responseJSON?.data?.message);
+            });
+        });
+
+    }
+
+    function customizerMenuRegister() {
+        $('.buymecoffee_customizer_menu').on("click", function () {
+            $('.buymecoffee_customizer_wrapper, .buymecoffee_edit_action_wrapper').toggle();
+            if (visibleCustomizer) {
+                $('.buymecoffee_main_quote').show();
+                $('.buymecoffee_edit_action_wrapper blockquote').hide();
+            } else {
+                $('.buymecoffee_main_quote').hide();
+                $('.buymecoffee_edit_action_wrapper blockquote').show();
+            }
+            visibleCustomizer = !visibleCustomizer;
+        });
+    }
+
+    function quotesEditRegister()
+    {
+        $('.buymecoffee_edit_action').on("click", function () {
+            $('.buymecoffee_edit_action_wrapper blockquote').toggle();
+            $('.buymecoffee_main_quote').toggle();
+        })
+
+        $('.buymecoffee_edit_action_wrapper input').on('input', function() {
+            $('.buymecoffee_main_quote p')[0]
+                .innerHTML = quote = $(this).val();
+        });
+    }
 
     function rgbToRgba(rgb, alpha) {
-        var values = rgb.substr(4, rgb.length - 5).split(", ");
+        let values = rgb.substr(4, rgb.length - 5).split(", ");
         return "rgba(" + values[0] + ", " + values[1] + ", " + values[2] + ", " + alpha + ")";
     }
 
     function hideSettings() {
-        $('.wrapper, .buymecoffee_edit_action_wrapper').hide();
-        $('.buymecoffee_quote_section .buymecoffee_main_quote').show();
+        visibleCustomizer = false;
+        $('.buymecoffee_customizer_wrapper, .buymecoffee_edit_action_wrapper').hide();
+        $('.buymecoffee_main_quote').show();
     }
 });
